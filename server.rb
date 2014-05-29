8dc63dd7adb75054c4f29282731196f07ca664e4
Adam Sheehan <a.t.sheehan@gmail.com>
2014-05-29 10:58:47 -0400

    Add form to submit questions.

--- server.rb
+++ server.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 require 'pg'
 
+def save_question(content)
+  sql = "INSERT INTO questions (content, created_at) " +
+    "VALUES ($1, NOW())"
+
+  connection = PG.connect(dbname: 'space_quora')
+  connection.exec_params(sql, [content])
+  connection.close
+end
+
 def find_questions
   connection = PG.connect(dbname: 'space_quora')
   results = connection.exec('SELECT * FROM questions')
@@ -12,4 +21,9 @@ end
 get '/questions' do
   @questions = find_questions
   erb :'questions/index.html'
+end
+
+post '/questions' do
+  save_question(params["content"])
+  redirect '/questions'
 end
